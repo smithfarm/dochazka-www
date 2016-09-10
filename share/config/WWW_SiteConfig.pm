ec5bfcea3db9566d160963f845aa1b2fd3fb0d94
Nathan Cutler <ncutler@suse.com>
2016-09-10 23:22:15 +0200

    WWW_SiteConfig.pm: force login credentials
    This is for testing. Comment it out to cause App::MFILE::WWW to display the
    login dialog.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- share/config/WWW_SiteConfig.pm
+++ share/config/WWW_SiteConfig.pm
@@ -2,3 +2,7 @@ set( 'MFILE_APPNAME', 'dochazka-www' );
 set( 'MFILE_WWW_LOG_FILE', $ENV{'HOME'} . '/dochazka-www.log' );
 set( 'MFILE_WWW_LOG_FILE_RESET', 1 );
 set( 'MFILE_WWW_BYPASS_LOGIN_DIALOG', 1 );
+set( 'MFILE_WWW_FORCE_LOGIN_CREDENTIALS', {
+    'nam' => 'root',
+    'pwd' => 'immutable',
+} )
