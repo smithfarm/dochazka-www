5c1394d33f9648511ef93a18ff6d7ff949d2fd8a
Nathan Cutler <ncutler@suse.com>
2016-09-15 20:36:13 +0200

    config: drop derived distro site config file
    Once a site config param is set, it is not possible to override it. This is a
    feature of App::CELL and it's not worth the effort to change it.
    
    As a result, we put all our site overrides in ext/WWW_SiteConfig.pm.example and
    rely on packagers to install it as /etc/dochazka-www/WWW_SiteConfig.pm.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- share/config/WWW_SiteConfig.pm
+++
@@ -1,9 +0,0 @@
-set( 'MFILE_APPNAME', 'dochazka-www' );
-set( 'DOCHAZKA_WWW_BACKEND_URI', 'http://localhost:5000' );
-set( 'MFILE_WWW_LOG_FILE', $ENV{'HOME'} . '/dochazka-www.log' );
-set( 'MFILE_WWW_LOG_FILE_RESET', 1 );
-set( 'MFILE_WWW_BYPASS_LOGIN_DIALOG', 0 );
-set( 'MFILE_WWW_DEFAULT_LOGIN_CREDENTIALS', {
-    'nam' => 'root',
-    'pwd' => 'immutable'
-} );
