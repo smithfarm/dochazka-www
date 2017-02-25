953d2713d1124a180601c762fe31b9bb46b460a0
Nathan Cutler <ncutler@suse.com>
2017-02-25 09:28:45 +0100

    0.143
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/Dochazka/WWW.pm
+++ lib/App/Dochazka/WWW.pm
@@ -48,11 +48,11 @@ App::Dochazka::WWW - Dochazka Attendance & Time Tracking system web client
 
 =head1 VERSION
 
-Version 0.142
+Version 0.143
 
 =cut
 
-our $VERSION = '0.142';
+our $VERSION = '0.143';
 
 
 
