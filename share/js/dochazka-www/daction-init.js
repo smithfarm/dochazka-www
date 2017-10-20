a0aa59df76f60abcd20bf5336c7ac1823cfbb33c
Nathan Cutler <ncutler@suse.com>
2017-10-21 00:17:28 +0200

    Revert "js: daction-init: fix pushable flag in one of the actions"
    This reverts commit 3ff55f70fb50565e603b800ecaab8b63446308d2.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- share/js/dochazka-www/daction-init.js
+++ share/js/dochazka-www/daction-init.js
@@ -137,7 +137,7 @@ define ([
             'menuText': 'Privilege (status) history',
             'aclProfile': 'passerby',
             'start': dactionStart('actionPrivHistory'),
-            'pushable': false
+            'pushable': true
         });
         target.push('actionPrivHistoryEdit', { // read-write
             'name': 'actionPrivHistoryEdit',
