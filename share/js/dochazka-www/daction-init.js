0eece9f32c724a3f0275451905ca0ddca8cbda55
Nathan Cutler <ncutler@suse.com>
2017-11-16 11:49:13 +0100

    js: hide inaccessible functionality from passerbies
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- share/js/dochazka-www/daction-init.js
+++ share/js/dochazka-www/daction-init.js
@@ -170,7 +170,7 @@ define ([
             'name': 'actionPrivHistory',
             'type': 'daction',
             'menuText': 'Status history',
-            'aclProfile': 'passerby',
+            'aclProfile': 'inactive',
             'start': dactionStart('actionPrivHistory'),
             // this starts the privhistory dtable, and if the dataset changes
             // we might want to unwind the stack to this action to reset that
